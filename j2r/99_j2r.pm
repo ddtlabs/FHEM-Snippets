609edc24553a6827e9609319fd4fe9343f41ec48
dev0 <ddtlabs@users.noreply.github.com>
2017-02-09 10:41:50 +0100

    Create 99_j2r.pm

---
+++ j2r/99_j2r.pm
@@ -0,0 +1,95 @@
+################################################################
+#
+#  Copyright (c) 2017 dev0
+#
+#  This script is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  The GNU General Public License can be found at
+#  http://www.gnu.org/copyleft/gpl.html.
+#  A copy is found in the textfile GPL.txt and important notices to the license
+#  from the author is found in LICENSE.txt distributed with these scripts.
+#
+#  This script is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  This copyright notice MUST APPEAR in all copies of the script!
+#
+################################################################
+
+# $Id: 99_j2r.pm 1 1970-01-101 00:00:00Z dev0 $
+
+package main;
+
+use strict;
+use warnings;
+use POSIX;
+
+sub
+j2r_Initialize($$) {
+  my ($hash) = @_;
+	Log3($hash, 3, "99_j2r.pm v1.0 (re)loaded");
+}
+
+
+sub j2r($$) {
+  my ($name,$event) = @_;
+  my $hash = $defs{$name};
+  my $type = $hash->{TYPE};
+  my $j = (split(": ",$event,2))[1];
+  my $h;
+
+	if ( not eval "use JSON; 1;" ) {
+    Log3 $name, 1, "$type $name: WARNING: Perl modul JSON is not installed.";
+    return "Perl module JSON is missing";
+  }
+
+  eval { $h = decode_json($j); 1; };
+  if ( $@ ) {
+    Log3 $name, 2, "$type $name: WARNING: deformed JSON data, check your config.";
+    Log3 $name, 2, "$type $name: $@";
+    return "deformed or no JSON";
+  }
+
+  readingsBeginUpdate($hash);
+  j2r_update($hash,$h);
+  readingsEndUpdate($hash, 1);
+
+  return undef;
+}
+
+sub j2r_update($$;$$) {
+  # thanx to bgewehr for this recursive snippet
+  # https://github.com/bgewehr/fhem
+  my ($hash,$ref,$prefix,$suffix) = @_;
+  $prefix = "" if( !$prefix );
+  $suffix = "" if( !$suffix );
+  $suffix = "_$suffix" if( $suffix );
+
+  if(  ref($ref) eq "ARRAY" ) {
+    while( my ($key,$value) = each $ref) {
+      j2r_update($hash,$value,$prefix.sprintf("%02i",$key+1)."_");
+    }
+  }
+  elsif( ref($ref) eq "HASH" ) {
+    while( my ($key,$value) = each $ref) {
+      if( ref($value) ) {
+        j2r_update($hash,$value,$prefix.$key.$suffix."_");
+      }
+      else {
+       readingsBulkUpdate($hash, j2r_clean($prefix).j2r_clean($key).j2r_clean($suffix), $value);
+      }
+    }
+  }
+}
+
+sub j2r_clean($) {
+  (my $r = @_[0]) =~ s/[^A-Za-z\d_\.\-\/]/_/g;
+  return $r;
+}
+
+1;
